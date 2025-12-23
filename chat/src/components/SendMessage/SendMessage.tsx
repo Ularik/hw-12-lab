0882dc4d38b039b05c155827ac202c1007f4efc4
Ular <eldarakashka@gmail.com>
2025-12-23 10:28:18 +0600

    Добавил отправку сообщений

--- chat/src/components/SendMessage/SendMessage.tsx
+++ chat/src/components/SendMessage/SendMessage.tsx
@@ -1,6 +1,12 @@
+import { useState, type FormEvent } from "react";
+
 const SendMessage = () => {
+    const [message, setMessage] = useState('');
+
     const url = "http://146.185.154.90:8000/messages";
-    const postMessage = async (message: string) => {
+    const postMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (message.trim() === '') return
         const data = {
             message: message,
             author: 'Joma'
@@ -18,10 +24,13 @@ const SendMessage = () => {
     return (
       <div className="container">
         <div className="d-flex justify-content-center">
-          <form action="#">
+          <form onSubmit={postMessage} action="#">
             <div className="row">
               <div className="col-9">
-                <input className="form-control" type="text" />
+                <input 
+                onChange={(e:React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)} 
+                value={message}
+                className="form-control" type="text" />
               </div>
               <button className="col-3 btn btn-dark">Send</button>
             </div>
